d330d0c6646a6df7ead394a86f4fe11c27c3ca88
Pedro Henrique Hoffmann da Cunha <phhcpedrohhcunha@gmail.com>
2024-03-06 19:05:16 -0300

    Create the dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,353 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Legend, Line, LineChart, Tooltip } from "recharts";
+import { ResponsiveContainer, BarChart, XAxis, YAxis, Bar } from "recharts";
+import { z } from "zod";
+
+interface Category {
+  id: number;
+  name: string;
+  products: Product[];
+}
+
+interface Product {
+  id: number;
+  name: string;
+  brands: Brand[];
+}
+
+interface Brand {
+  id: number;
+  name: string;
+  sales: MonthSale[];
+}
+
+interface MonthSale {
+  month: string;
+  amount: number;
+}
+
+const categories: Category[] = [
+  {
+    id: 1,
+    name: "Electronics",
+    products: [
+      {
+        id: 1,
+        name: "Laptop",
+        brands: [
+          {
+            id: 1,
+            name: "Dell",
+            sales: [
+              { month: "Jan", amount: 5000 },
+              { month: "Feb", amount: 6000 },
+              { month: "Mar", amount: 7500 },
+              { month: "Apr", amount: 8000 },
+            ],
+          },
+          {
+            id: 2,
+            name: "HP",
+            sales: [
+              { month: "Jan", amount: 4500 },
+              { month: "Feb", amount: 5500 },
+              { month: "Mar", amount: 7000 },
+              { month: "Apr", amount: 7500 },
+            ],
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Smartphone",
+        brands: [
+          {
+            id: 1,
+            name: "Samsung",
+            sales: [
+              { month: "Jan", amount: 8000 },
+              { month: "Feb", amount: 9000 },
+              { month: "Mar", amount: 10000 },
+              { month: "Apr", amount: 11000 },
+            ],
+          },
+          {
+            id: 2,
+            name: "Apple",
+            sales: [
+              { month: "Jan", amount: 12000 },
+              { month: "Feb", amount: 13000 },
+              { month: "Mar", amount: 14000 },
+              { month: "Apr", amount: 15000 },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Clothing",
+    products: [
+      {
+        id: 1,
+        name: "T-Shirt",
+        brands: [
+          {
+            id: 1,
+            name: "Nike",
+            sales: [
+              { month: "Jan", amount: 2000 },
+              { month: "Feb", amount: 2500 },
+              { month: "Mar", amount: 3000 },
+              { month: "Apr", amount: 3500 },
+            ],
+          },
+          {
+            id: 2,
+            name: "Adidas",
+            sales: [
+              { month: "Jan", amount: 1800 },
+              { month: "Feb", amount: 2200 },
+              { month: "Mar", amount: 2800 },
+              { month: "Apr", amount: 3200 },
+            ],
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Jeans",
+        brands: [
+          {
+            id: 1,
+            name: "Levi's",
+            sales: [
+              { month: "Jan", amount: 3000 },
+              { month: "Feb", amount: 3500 },
+              { month: "Mar", amount: 4000 },
+              { month: "Apr", amount: 4500 },
+            ],
+          },
+          {
+            id: 2,
+            name: "Wrangler",
+            sales: [
+              { month: "Jan", amount: 2500 },
+              { month: "Feb", amount: 3000 },
+              { month: "Mar", amount: 3500 },
+              { month: "Apr", amount: 4000 },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const FormSchema = z.object({
+  selectedCategoryId: z.number().optional(),
+  selectedProductId: z.number().optional(),
+  selectedBrandId: z.number().optional(),
+});
 
 export default function Home() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const { selectedCategoryId, selectedProductId, selectedBrandId } =
+    form.watch();
+
+  const selectedCategory = categories.find(
+    (category) => category.id === selectedCategoryId
+  );
+  const selectedProduct = selectedCategory?.products.find(
+    (product) => product.id === selectedProductId
+  );
+  const selectedBrand = selectedProduct?.brands.find(
+    (brand) => brand.id === selectedBrandId
+  );
+
+  const totalSales =
+    selectedBrand?.sales.reduce((total, sale) => total + sale.amount, 0) || 0;
+
+  const data = selectedBrand?.sales.reduce((acc, sale) => {
+    acc.push({
+      name: sale.month,
+      total: sale.amount,
+    });
+    return acc;
+  }, [] as { name: string; total: number }[]);
+
+  console.log(
+    { selectedCategory, selectedProduct, selectedBrand },
+    { totalSales }
+  );
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+      <div className="flex flex-col space-y-4">
+        <Form {...form}>
+          <div className="flex space-x-4">
+            <FormField
+              control={form.control}
+              name="selectedCategoryId"
+              render={({ field }) => (
+                <FormItem className="w-64">
+                  <FormLabel>Category</FormLabel>
+                  <Select
+                    onValueChange={(value) => field.onChange(Number(value))}
+                    defaultValue={field.value?.toString()}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a category" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {categories.map((category) => (
+                        <SelectItem
+                          key={category.id}
+                          value={category.id.toString()}
+                        >
+                          {category.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
             />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <FormField
+              control={form.control}
+              name="selectedProductId"
+              disabled={!selectedCategory}
+              render={({ field }) => (
+                <FormItem className="w-64">
+                  <FormLabel>Product</FormLabel>
+                  <Select
+                    onValueChange={(value) => field.onChange(Number(value))}
+                    defaultValue={field.value?.toString()}
+                    disabled={!selectedCategory}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a product" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {selectedCategory?.products.map((product) => (
+                        <SelectItem
+                          key={product.id}
+                          value={product.id.toString()}
+                        >
+                          {product.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <FormField
+              control={form.control}
+              name="selectedBrandId"
+              disabled={!selectedProduct}
+              render={({ field }) => (
+                <FormItem className="w-64">
+                  <FormLabel>Brand</FormLabel>
+                  <Select
+                    onValueChange={(value) => field.onChange(Number(value))}
+                    defaultValue={field.value?.toString()}
+                    disabled={!selectedProduct}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a brand" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {selectedProduct?.brands.map((brand) => (
+                        <SelectItem key={brand.id} value={brand.id.toString()}>
+                          {brand.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+          </div>
+        </Form>
+        <Card>
+          <CardHeader>
+            <CardTitle>Monthly Sales</CardTitle>
+            <CardDescription>
+              Monthly sales for the selected brand
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="h-96 p-4">
+            {!selectedBrand ? (
+              <p className="p-4 text-center text-slate-400">
+                Select a brand to view sales
+              </p>
+            ) : (
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                  data={data}
+                  margin={{
+                    top: 10,
+                    right: 30,
+                    left: 20,
+                    bottom: 10,
+                  }}
+                >
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Line
+                    type="monotone"
+                    dataKey="total"
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            )}
+          </CardContent>
+        </Card>
+        <span className="text-sm text-right text-slate-600 ">
+          Total Sales:{" "}
+          {totalSales.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}
+        </span>
       </div>
     </main>
   );
